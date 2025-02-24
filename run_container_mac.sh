2c99fab3492b6dd63d97e2673bc584ea5cc19c84
Nick-2003 <tsunlinicholastam@gmail.com>
2025-02-23 21:29:23 -0800

    Comments kept as options

--- run_container_mac.sh
+++ run_container_mac.sh
@@ -5,7 +5,13 @@ docker run --platform=linux/amd64 --rm -p 10000:8888 \
     kayleeli/dsci310_g6_milestone1 \
     start-notebook.sh --NotebookApp.token=''
 
+# docker run --platform=linux/amd64 --rm -p 10000:8888 \
+#     -v "$(pwd)/src":/home/jovyan/work/src \
+#     -v "$(pwd)/data":/home/jovyan/work/data \
+#     nt2103/dsci310practice1 \
+#     start-notebook.sh --NotebookApp.token=''
+
 # docker run --rm -it -p 10000:8888 \ 
 #     -v "$(pwd)/src":/home/jovyan/work/src \
 #     -v "$(pwd)/data":/home/jovyan/work/data \
-#     -e DISABLE_AUTH=true nt2103/dsci310practice1 
+#     -e DISABLE_AUTH=true nt2103/dsci310practice1
