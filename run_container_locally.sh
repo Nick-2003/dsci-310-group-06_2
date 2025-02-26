b14bbe414bc17c1ed34026f6bda605a8ba5535fa
Kayleeli <106621543+Kayleeli@users.noreply.github.com>
2025-02-26 01:03:56 -0800

    Update run_container_locally.sh

--- run_container_locally.sh
+++ run_container_locally.sh
@@ -1,5 +1,6 @@
 docker run --rm -p 10000:8888 \
   -v /$(pwd)/reports:/home/jovyan/work/reports \
   -v /$(pwd)/data:/home/jovyan/work/data \
+  -v /$(pwd)/src:/home/jovyan/work/src \
   kayleeli/dsci310_g6_milestone1 \
   start-notebook.sh --NotebookApp.token=''
